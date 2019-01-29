38d92963f23e3bfe116ebcaff091456020de20f2
wafflez4421 <45349198+wafflez4421@users.noreply.github.com>
2019-01-29 01:04:54 -0500

    Add files via upload

---
+++ new/src/bitcoin/TestVerifyValueBlank.java
@@ -0,0 +1,36 @@
+package bitcoin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(BitcoinInPriceService.class)
+public class TestVerifyValueBlank {
+
+	@Test
+	public void testVerifyValueBlank() {
+		// Set up
+		String nbrBitcoin = " ";
+		BitcoinWallet bWallet = new BitcoinWallet();
+
+		// Build
+		bWallet.verifyValue(nbrBitcoin);
+
+		// Exercise
+		assertFalse(bWallet.verifyValue(nbrBitcoin));
+	}
+
+}
---
+++ new/src/bitcoin/TestVerifyValuePositive.java
@@ -0,0 +1,14 @@
+package bitcoin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestVerifyValuePositive {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
